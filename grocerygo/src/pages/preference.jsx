3d28a7254a20e8169f465a0558b7c4d708a0de64
Zachary Chua <zacharychuaze@yahoo.com>
2023-09-12 20:42:23 +0800

    Update preference.jsx

--- grocerygo/src/pages/preference.jsx
+++ grocerygo/src/pages/preference.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect} from "react";
-import { View, Text, ScrollView, TouchableOpacity } from "react-native";
+import React, { useState} from "react";
+import { View, Text, ScrollView, TouchableOpacity, TextInput, KeyboardAvoidingView } from "react-native";
 
 const preferences = [
   "Gluten-Free",
@@ -32,9 +32,8 @@ const PreferenceButton = ({ preference, isSelected, onPress }) => {
 const Preference = () => {
   // declare vars and set their state
   const [selectedPreferences, setSelectedPreferences] = useState([]);
-  const [submittedPreferences, setSubmittedPreferences] = useState([]);
-  const [showButtons, setShowButtons] = useState(false);
-
+  // const [submittedPreferences, setSubmittedPreferences] = useState([]);
+  const [customPreference, setCustomPreference] = useState(""); // State for custom preference input
   const togglePreference = (preference) => {
     if (selectedPreferences.includes(preference)) {
       setSelectedPreferences(
@@ -43,65 +42,78 @@ const Preference = () => {
     } else {
       setSelectedPreferences([...selectedPreferences, preference]);
     }
-    setShowButtons(true);
   };
 
   // Function to handle the "Submit" button click
   const handleSubmit = () => {
-    // Store the current selected preferences as submitted preferences
-    setSubmittedPreferences([...selectedPreferences]);
-    // Do something with selectedPreferences (e.g., send to server, update state, etc.)
-    console.log("Selected Preferences:", selectedPreferences);
-    setShowButtons(false);
+      setSelectedPreferences((prevSelectedPreferences) => {
+        // Store the current selected preferences as submitted preferences
+        let finalPreferences = [...prevSelectedPreferences];
+
+        // If customPreference is not empty, add it to the selected preferences
+        if (customPreference.trim() !== "") {
+          finalPreferences.push(customPreference);
+          setCustomPreference(""); // Clear the input field
+        }
+
+        console.log("Submitted Preferences:", finalPreferences);
+        return finalPreferences;
+      });
+
   };
 
-  // Function to handle the "Cancel" button click
-  const handleCancel = () => {
-    // Clear selected preferences and hide the buttons
-    console.log('Previous Preferences:', submittedPreferences)
-    setSelectedPreferences([...submittedPreferences]);
-    console.log("Selected Preferences:", selectedPreferences)
-    setShowButtons(false);
+  const handleCustomPreferenceChange = (text) => {
+    setCustomPreference(text);
   };
+
 
   return (
-    <ScrollView className="flex-1 bg-main-background">
-      <View className="flex-1 m-2">
-        <Text className="text-3xl font-bold text-black px-4">
-          Product Preferences
-        </Text>
-        <Text className="text-base font-medium text-black px-4 pt-2">
-          We will recommend products based on your needs.
-        </Text>
-      </View>
-      <View className="mt-2 ">
-        {preferences.map((preference, index) => (
-          <PreferenceButton
-            key={index}
-            preference={preference}
-            isSelected={selectedPreferences.includes(preference)}
-            onPress={togglePreference}
-          />
-        ))}
-      </View>
-      {/* Conditional rendering of buttons */}
-      {selectedPreferences.length > 0 && showButtons && (
+    <KeyboardAvoidingView
+      behavior="padding"
+      enabled
+      keyboardVerticalOffset={100}
+      className="flex-1 bg-main-background"
+    >
+      <ScrollView className="flex-1 bg-main-background">
+        <View className="flex-1 m-2">
+          <Text className="text-3xl font-bold text-black px-4">
+            Product Preferences
+          </Text>
+          <Text className="text-base font-medium text-black px-4 pt-2">
+            We will recommend products based on your needs.
+          </Text>
+        </View>
+        <View className="mt-2 ">
+          {preferences.map((preference, index) => (
+            <PreferenceButton
+              key={index}
+              preference={preference}
+              isSelected={selectedPreferences.includes(preference)}
+              onPress={togglePreference}
+            />
+          ))}
+        </View>
+        <Text className = 'text-xl font-bold m-3'>Any Other Allergies/Preferences?</Text>
+        <TextInput
+          placeholder="Add Custom Preference"
+          value={customPreference}
+          onChangeText={handleCustomPreferenceChange}
+          className="border-2 border-black rounded-lg m-3 p-5"
+
+        />
+        {/* Conditional rendering of buttons */}
         <View className=" mt-2 flex flex-row justify-around">
           <TouchableOpacity
-            className="p-4 border-black border-2 rounded-lg bg-main-green "
+            className="p-4 border-black border-2  bg-main-green "
             onPress={handleSubmit}
           >
-            <Text className="text-xl font-bold text-white ">Submit</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            className="p-4 border-black border-2 rounded-lg bg-main-red"
-            onPress={handleCancel}
-          >
-            <Text className="text-xl font-bold text-white ">Cancel</Text>
+            <Text className="text-xl font-bold text-white ">
+              Submit Preferences
+            </Text>
           </TouchableOpacity>
         </View>
-      )}
-    </ScrollView>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
 

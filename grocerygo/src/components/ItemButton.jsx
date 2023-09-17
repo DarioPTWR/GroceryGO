abb0da473e79ac91e8c6fcdad396bbd49b651659
Dario Prawara Teh <112337659+DarioPTWR@users.noreply.github.com>
2023-09-17 16:08:13 +0800

    Added new jsx files for button and back button for Item page

---
+++ grocerygo/src/components/ItemButton.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {View, Text, Pressable} from 'react-native'
+
+const ItemButton = ({onPress,buttonText}) => {
+    return (
+        <Pressable onPress={onPress} className='items-center'>
+            <View style={{width: 310}} className="bg-[#355D4E] py-2 mt-2 rounded-md">
+                <Text style={{fontSize: 16}} className="text-white text-md text-center">{buttonText}</Text>
+            </View>
+        </Pressable>
+    )
+}
+
+export default ItemButton

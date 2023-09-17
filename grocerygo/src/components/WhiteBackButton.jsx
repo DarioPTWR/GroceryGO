e40f69f7f26f488c4240129ce8127b654e5b88f5
Zzyy <72967431+ZhnYng@users.noreply.github.com>
2023-09-17 17:45:39 +0800

    working image

--- grocerygo/src/components/WhiteBackButton.jsx
+++ grocerygo/src/components/WhiteBackButton.jsx
@@ -1,18 +1,23 @@
-import {Pressable} from "react-native";
-import {AntDesign} from "@expo/vector-icons";
+import { Pressable } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
 import React from "react";
 
-const WhiteBackButton = ({navigation}) => {
-    return(
-        <Pressable
-         className="self-start ml-4"
-         onPress={()=>{
-            navigation.goBack();
-         }}
-         >
-         <AntDesign name='arrowleft'  style={{marginTop: -240, marginLeft: 5}} size={20} color='white'></AntDesign>
-        </Pressable>
-    )
-}
+const WhiteBackButton = ({ navigation }) => {
+  return (
+    <Pressable
+      className="self-start ml-4"
+      onPress={() => {
+        navigation.goBack();
+      }}
+    >
+      <AntDesign
+        name="arrowleft"
+        style={{ marginTop: -240, marginLeft: 5 }}
+        size={20}
+        color="white"
+      ></AntDesign>
+    </Pressable>
+  );
+};
 
 export default WhiteBackButton;
